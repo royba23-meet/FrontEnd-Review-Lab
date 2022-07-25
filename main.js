557d9bcab03b12cc6b0d747491c98159d6637bf2
royba23-meet <roybacharschool@gmail.com>
2022-07-25 12:40:59 +0300

    Added files, very lazy implementation

---
+++ main.js
@@ -0,0 +1,7 @@
+function changeBackgroundColor() {
+    if (document.body.style.backgroundColor == "white") {
+        document.body.style.backgroundColor = "black";
+    }
+    else 
+    {document.body.style.backgroundColor = "white";}
+}
